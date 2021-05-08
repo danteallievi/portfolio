e9e61469b0199c01f470b136db472c4931ddec2d
Dante Allievi <toto99.-@hotmail.com>
2021-05-08 11:16:31 +0200

    Skill added

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,5 @@
-import { Container, Typography, Grid } from "@material-ui/core";
-import useStyles from "./styles";
+import { Container, Typography, Grid } from '@material-ui/core';
+import useStyles from './styles';
 
 const Skills = () => {
   const classes = useStyles();
@@ -8,17 +8,17 @@ const Skills = () => {
   return (
     <>
       <Container className={classes.container}>
-        <Typography variant="h3" gutterBottom className={classes.title}>
+        <Typography variant='h3' gutterBottom className={classes.title}>
           Relevant skills
         </Typography>
 
         <Grid
           className={classes.grid}
           container
-          direction="row"
-          alignItems="flex-start"
-          alignContent="center"
-          justify="space-around"
+          direction='row'
+          alignItems='flex-start'
+          alignContent='center'
+          justify='space-around'
         >
           <Grid
             className={classes.parent}
@@ -27,47 +27,47 @@ const Skills = () => {
             xs={12}
             sm={6}
             md={4}
-            alignItems="center"
-            alignContent="center"
-            justify="center"
+            alignItems='center'
+            alignContent='center'
+            justify='center'
           >
             <div className={classes.paper}>
-              <Typography variant="h4" gutterBottom>
+              <Typography variant='h4' gutterBottom>
                 Front-End
               </Typography>
               <div className={classes.content}>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     React.js / Redux
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     CSS / Sass
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     Bootstrap / Reactstrap
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     Responsive Design
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     Material UI
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     Styled Components
                   </Typography>
                 </Grid>
@@ -82,29 +82,29 @@ const Skills = () => {
             xs={12}
             sm={6}
             md={4}
-            alignItems="center"
-            alignContent="center"
-            justify="center"
+            alignItems='center'
+            alignContent='center'
+            justify='center'
           >
             <div className={classes.paper}>
-              <Typography variant="h4" gutterBottom>
+              <Typography variant='h4' gutterBottom>
                 Back-End
               </Typography>
               <div className={classes.content}>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     Node.js
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     Express.js
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     MongoDB
                   </Typography>
                 </Grid>
@@ -119,35 +119,41 @@ const Skills = () => {
             xs={12}
             sm={6}
             md={4}
-            alignItems="center"
-            alignContent="center"
-            justify="center"
+            alignItems='center'
+            alignContent='center'
+            justify='center'
           >
             <div className={classes.paper}>
-              <Typography variant="h4" gutterBottom>
+              <Typography variant='h4' gutterBottom>
                 Others
               </Typography>
               <div className={classes.content}>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
+                    Typescript
+                  </Typography>
+                  <Line />
+                </Grid>
+                <Grid item>
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     Git / GitHub
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
-                    Unit Testing
+                  <Typography className={classes.subtitle} variant='subtitle1'>
+                    Testing
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     Terminal
                   </Typography>
                   <Line />
                 </Grid>
                 <Grid item>
-                  <Typography className={classes.subtitle} variant="subtitle1">
+                  <Typography className={classes.subtitle} variant='subtitle1'>
                     ESLint
                   </Typography>
                 </Grid>
