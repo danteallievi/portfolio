b99f5c0b841b076fc1b60655b621039a06a11713
Dante Allievi <toto99.-@hotmail.com>
2021-05-13 21:27:54 +0200

    Sidebar updated, resume added.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,41 +1,41 @@
-import GitHubIcon from "@material-ui/icons/GitHub";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import MailIcon from "@material-ui/icons/Mail";
-// import PermContactCalendarIcon from "@material-ui/icons/PermContactCalendar";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailIcon from '@material-ui/icons/Mail';
+import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
 
-import useStyle from "./styles";
+import useStyle from './styles';
 
 const Sidebar = () => {
   const classes = useStyle();
   return (
     <>
       <aside className={classes.sidebar}>
-        <a href="https://www.linkedin.com/in/dante-allievi-3907291b2">
+        <a href='https://www.linkedin.com/in/dante-allievi-3907291b2'>
           <span className={classes.text}>Linkedin</span>
           <i>
             <LinkedInIcon className={classes.icon} />
           </i>
         </a>
 
-        <a href="https://github.com/danteallievi">
+        <a href='https://github.com/danteallievi'>
           <span className={classes.text}>Github</span>
           <i>
             <GitHubIcon className={classes.icon} />
           </i>
         </a>
 
-        <a href="mailto:danteallievi@gmail.com">
+        <a href='mailto:danteallievi@gmail.com'>
           <span className={classes.text}>Email</span>
           <i>
             <MailIcon className={classes.icon} />
           </i>
         </a>
-        {/* <a href="#">
+        <a href='https://drive.google.com/file/d/1feI_RfPuSpKapAKn2yIY_W0QWAcB5_Ju/view?usp=sharing'>
           <span className={classes.text}>Resume</span>
           <i>
             <PermContactCalendarIcon className={classes.icon} />
           </i>
-        </a> */}
+        </a>
       </aside>
     </>
   );
