e4b826aae8c789bfd14c2dd5cd939f46733b1f7a
Dante Allievi <toto99.-@hotmail.com>
2021-05-13 21:24:35 +0200

    Proyect authentication added.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,8 +5,9 @@ import Project from './Proyect/Project';
 
 import memoriesImg from '../../images/memories.jpg';
 import eCommerceImg from '../../images/e-commerce.png';
-import breakingBadImg from '../../images/BreakingBad.jpg';
 import expenseTrackerImg from '../../images/expenseTracker.jpg';
+import authappImg from '../../images/authapp.jpg';
+import breakingBadImg from '../../images/BreakingBad.jpg';
 
 const memories = {
   text: `For this project I implemented the MERN-STACK for the first time, taking care of the APP from start to finish. Adding authentication, to allow the user to register and log in with email, or register using their google account, to like, create or delete a post`,
@@ -18,15 +19,20 @@ const eCommerce = {
   demo: `https://tech-e-commerce.netlify.app/`,
   code: `https://github.com/danteallievi/e-commerce`,
 };
-const breakingBad = {
-  text: `For this project I've worked in React.js, with a Breaking Bad API, to create a small app with a simple pagination, to show and filter the character info.`,
-  demo: `https://danteallievi.github.io/breakingbad/`,
-  code: `https://github.com/danteallievi/breakingbad`,
+const authapp = {
+  text: `For this project, built in React.js, I used the firebase service. To practice mail authentication on the front side.`,
+  demo: `https://auth-app-demo.netlify.app/`,
+  code: `https://github.com/danteallievi/firebase-authentication`,
 };
 const expenseTracker = {
   text: `This was one of my first apps. Built in React.js to learn the correct use of React-Hooks`,
   demo: `https://danteallievi.github.io/expense-tracker/`,
   code: `https://github.com/danteallievi/expense-tracker`,
+};
+const breakingBad = {
+  text: `For this project I've worked in React.js, with a Breaking Bad API, to create a small app with a simple pagination, to show and filter the character info.`,
+  demo: `https://danteallievi.github.io/breakingbad/`,
+  code: `https://github.com/danteallievi/breakingbad`,
 };
 
 const Portfolio = () => {
@@ -76,6 +82,15 @@ const Portfolio = () => {
           image={eCommerceImg}
           demoLink={eCommerce.demo}
           codeLink={eCommerce.code}
+        />
+
+        <Project
+          title='AUTHENTICATION APP'
+          technologies={['REACT.JS', 'REACT-HOOKS', 'FIREBASE', 'BOOTSTRAP']}
+          text={authapp.text}
+          image={authappImg}
+          demoLink={authapp.demo}
+          codeLink={authapp.code}
         />
 
         <Project
