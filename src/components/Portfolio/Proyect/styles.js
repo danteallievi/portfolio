8e95a0e988b0578156f16a2b6c39a09ae6fa846a
Dante Allievi <toto99.-@hotmail.com>
2021-05-19 11:33:18 +0200

    Image memories changed

--- src/components/Portfolio/Proyect/styles.js
+++ src/components/Portfolio/Proyect/styles.js
@@ -1,47 +1,47 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles((theme) => ({
+export default makeStyles(theme => ({
   toolbar: theme.mixins.toolbar,
 
   box: {
-    width: "100%",
-    height: "100%",
-    position: "relative",
-    margin: "2rem 1rem 0 1rem",
-    transition: "all 0.5s",
-    boxShadow: "0px 0px 20px 0px #000000",
-    borderRadius: "2rem",
-    padding: "1rem",
+    width: '100%',
+    height: '100%',
+    position: 'relative',
+    margin: '2rem 1rem 0 1rem',
+    transition: 'all 0.5s',
+    boxShadow: '0px 0px 20px 0px #000000',
+    borderRadius: '2rem',
+    padding: '1rem',
 
-    "& button": {
-      top: "80%",
-      backgroundColor: "#f3f3f3",
-      opacity: "0",
+    '& button': {
+      top: '80%',
+      backgroundColor: '#f3f3f3',
+      opacity: '0',
     },
-    "&:hover": {
-      "& button": {
-        transition: "all 0.5s",
-        opacity: "1",
-        cursor: "pointer",
+    '&:hover': {
+      '& button': {
+        transition: 'all 0.5s',
+        opacity: '1',
+        cursor: 'pointer',
       },
-      "& img": {
-        opacity: "0.5",
+      '& img': {
+        opacity: '0.5',
       },
     },
 
-    [theme.breakpoints.down("sm")]: {
-      padding: "0.5rem",
+    [theme.breakpoints.down('sm')]: {
+      padding: '0.5rem',
     },
   },
   img: {
-    borderRadius: "2rem",
+    borderRadius: '2rem',
   },
   projectButtons: {
-    top: "90%",
-    position: "absolute",
+    top: '90%',
+    position: 'absolute',
 
-    [theme.breakpoints.down("xs")]: {
-      fontSize: "0.7rem",
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '0.7rem',
     },
   },
 
