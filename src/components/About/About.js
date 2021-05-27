455eb5869de6e831d95b5f3cd86ae74aee4c3f9c
Dante Allievi <danteallievi@gmail.com>
2021-05-27 11:52:54 +0200

    Test

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,34 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import { Container, Typography, Button } from "@material-ui/core";
-import useStyles from "./styles";
+import { Container, Typography, Button } from '@material-ui/core';
+import useStyles from './styles';
 
 const About = () => {
   const classes = useStyles();
 
   return (
     <>
-      <Container className={classes.container} align="center">
-        <Typography variant="h2" gutterBottom>
+      <Container className={classes.container} align='center'>
+        <Typography variant='h2' gutterBottom>
           HI THERE. IT'S DANTE ALLIEVI
         </Typography>
-        <Typography variant="subtitle1" className={classes.subtitle}>
+        <Typography variant='subtitle1' className={classes.subtitle}>
           I'M A WEB DESIGNER & DEVELOPER CURRENTLY BASED IN HANNOVER GERMANY.
         </Typography>
-        <Typography className={classes.subtitle} variant="subtitle1" paragraph>
+        <Typography className={classes.subtitle} variant='subtitle1' paragraph>
           HIT ME UP AND LET'S WORK TOGETHER
         </Typography>
 
         <Button
           component={Link}
-          to="/portfolio"
+          to='/portfolio'
           className={classes.button}
-          color="default"
-          variant="contained"
+          color='default'
+          variant='contained'
           disableElevation
-          size="small"
+          size='small'
         >
-          VIEW PROJECTS
+          VIEW PROJECTS!
         </Button>
       </Container>
     </>
