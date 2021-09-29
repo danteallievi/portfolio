b19204013c533a9b6de557cb56a7749c60eef8fa
Dante Allievi <danteallievi@gmail.com>
2021-09-29 16:03:25 +0200

    Scrollbar changed

--- src/components/Portfolio/styles.js
+++ src/components/Portfolio/styles.js
@@ -1,26 +1,26 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   container: {
-    minHeight: "calc(100vh - 10vh)",
+    minHeight: 'calc(100vh - 10vh)',
   },
 
   title: {
-    paddingTop: "6rem",
+    paddingTop: '6rem',
 
-    [theme.breakpoints.down("md")]: {
-      paddingTop: "4rem",
+    [theme.breakpoints.down('md')]: {
+      paddingTop: '4rem',
     },
-    [theme.breakpoints.down("xs")]: {
-      paddingTop: "3rem",
+    [theme.breakpoints.down('xs')]: {
+      paddingTop: '3rem',
     },
   },
   gris: {
-    color: "#ced4da",
+    color: '#ced4da',
     fontSize: 18,
   },
   grid: {
     flexGrow: 1,
-    marginBottom: "1rem",
+    marginBottom: '1rem',
   },
 }));
--- src/styles.js
+++ src/styles.js
@@ -1,15 +1,35 @@
-import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 
 let theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '*': {},
+        '*::-webkit-scrollbar': {
+          width: '10px',
+        },
+        '*::-webkit-scrollbar-thumb': {
+          'background-color': '#8a8a8a',
+          outline: '2px solid #fff',
+          'outline-offset': '-2px',
+          border: '0.1px solid #b7b7b7',
+        },
+        '*::-webkit-scrollbar-thumb:hover': {
+          'background-color': '#5c5c5c',
+        },
+        '*::-webkit-scrollbar-track-piece': {
+          'background-color': '#fefefe',
+        },
+      },
+    },
+  },
   palette: {
     background: {
-      default: "#222222",
+      default: '#222222',
     },
     text: {
-      primary: "#f3f3f3",
+      primary: '#f3f3f3',
     },
   },
 });
 export default theme = responsiveFontSizes(theme);
-
-
